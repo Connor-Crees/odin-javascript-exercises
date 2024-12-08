5a86f9e0121faee3db2b6cd6d707c75cdd5a0308
Connor-Crees <connor.crees@gmail.com>
2024-12-08 17:16:17 +0000

    completed 09_palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    //input clean up
+    let cleanedString = string.toLowerCase();
+    const availableCharacters = "qwertyuiopasdfghjklzxcvbnm1234567890";
+    cleanedString = cleanedString.split('').filter((char) => availableCharacters.includes(char)).join('');
+    console.log(cleanedString);
 
+    const reverse = cleanedString.split('').reverse().join('');
+
+    return (cleanedString == reverse);
 };
 
 // Do not edit below this line
