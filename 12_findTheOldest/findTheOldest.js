029fe0dd7ee55e6afdde4dc18d60087aed46225a
Connor-Crees <connor.crees@gmail.com>
2024-12-08 19:07:18 +0000

    completed 12_findTheOldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,24 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((oldestPerson, currentPerson) => {
+        const oldestAge = findAge(oldestPerson);
+        const currentAge = findAge(currentPerson);
+        if(oldestAge < currentAge){
+            return currentPerson;
+        }
+        else{
+            return oldestPerson;
+        }
+    })
 };
+
+const findAge = function(person){
+    let death = person.yearOfDeath;
+    if(!death){
+        death = new Date().getFullYear();
+    }
+    let birth = person.yearOfBirth;
+    return (death - birth);
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
