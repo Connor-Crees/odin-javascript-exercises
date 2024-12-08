2130ba396637eea0b71313e644fe9771f3944be6
Connor-Crees <connor.crees@gmail.com>
2024-12-08 17:22:22 +0000

    completed 10_fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if(n < 0){
+        return "OOPS";
+    }
+    if(n == 0){
+        return 0;
+    }
+    if(n <= 2){
+        return 1;
+    }
+    let a = 1;
+    let b = 1;
+    for(i = 3; i <= n; i++){
+        temp = a;
+        a = b;
+        b = b + temp;
+    }
+    return b;
 };
 
 // Do not edit below this line
